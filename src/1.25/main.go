c0e59eb2d63ff841b7e52c5398396505da4a3fdd
lonmouth <dmitri.khramtsov@gmail.com>
2025-09-06 02:17:59 +0300

    FIN_0.1

--- src/1.25/main.go
+++ src/1.25/main.go
@@ -5,12 +5,40 @@ import (
 	"time"
 )
 
-func mySleep(d time.Duration) {
-	<-time.After(d)
+func mySleep1(d time.Duration) {
+	done := make(chan struct{})
+
+	go func() {
+		<-time.After(d)
+		close(done)
+	}()
+
+	<-done
+}
+
+func mySleep2(t time.Duration) {
+	done := make(chan struct{})
+	go func() {
+		start := time.Now()
+		for time.Since(start) < t {
+		}
+		close(done)
+	}()
+	<-done
+}
+
+func mySleep3(t time.Duration) {
+	select {
+	case <-time.After(t):
+	}
 }
 
 func main() {
 	fmt.Println("Start")
-	mySleep(2 * time.Second)
-	fmt.Println("End")
+	mySleep1(2 * time.Second)
+	fmt.Println("End1")
+	mySleep2(2 * time.Second)
+	fmt.Println("End2")
+	mySleep3(2 * time.Second)
+	fmt.Println("End3")
 }
